f5aa8d2402aeeb7c4d85926b3c06e454f72121cc
ruiruirui-1 <2537855826@qq.com>
2024-07-18 13:58:24 +0800

    Update Test.java

--- Sea/Test.java
+++ Sea/Test.java
@@ -8,11 +8,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		//Autoship2 a1 = new Autoship2();
 		//a1.setcru();a1.setdes();a1.setair();
-		//a1.print();
-		ArrayList<String> s = new ArrayList<String>();
-		
-		System.out.println(s.get(0));
-		
+		//a1.print();		
 	}
 
 }
